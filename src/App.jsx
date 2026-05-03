d37b4e4e1548ce415ef9c9bb8f583b798a15b15e
Flipps12 <pagianasypruebas12@gmail.com>
2026-05-03 13:10:07 -0300

    basic ui

--- src/App.jsx
+++ src/App.jsx
@@ -38,9 +38,9 @@ function App() {
   async function sendCommand() {
     try {
       // Llamamos al comando unificado en Rust
-      await invoke("send_knot_command", { 
-        command: command, 
-        args: arg ? [arg] : [] 
+      await invoke("send_knot_command", {
+        command: command,
+        args: arg ? [arg] : []
       });
     } catch (error) {
       console.error("Error en Knot:", error);
@@ -49,44 +49,59 @@ function App() {
   }
 
   return (
-    <main className="container">
-      <h1>Knot Chat Terminal</h1>
-
-      <div className="status-bar">
-        <span>RTT: {rtt !== null ? `${rtt}ms` : "---"}</span>
-      </div>
-
+    <main className="w-screen h-screen bg-mist-950 text-white">
+      <h1 className="h-[8%] text-white font-bold text-3xl p-4 border-b border-white">Knot-chat</h1>
+      <div className="h-[85%]"></div>
       <form
-        className="row"
         onSubmit={(e) => {
           e.preventDefault();
           sendCommand();
         }}
-      >
-        <select value={command} onChange={(e) => setCommand(e.target.value)}>
-          <option value="status">Status</option>
-          <option value="version">Version</option>
-          <option value="connect">Connect (Addr)</option>
-          <option value="ping">Ping (PeerId)</option>
-        </select>
-
-        <input
-          onChange={(e) => setArg(e.currentTarget.value)}
-          placeholder="Argumento (Address/ID)..."
-          value={arg}
-        />
-        <button type="submit">Enviar</button>
+        className="h-[7%]">
+        <div className="p-2 px-4 bg-mist-900 rounded-2xl flex flex-row text-white">
+          <input type="text" className="w-full h-12 outline-0" placeholder="Message" />
+          <button type="submit" className="h-12 outline-0">Send</button>
+        </div>
       </form>
-
-      <div className="response-log">
-        <h3>Historial de red:</h3>
-        <ul>
-          {responses.map((res, i) => (
-            <li key={i}><code>{res}</code></li>
-          ))}
-        </ul>
-      </div>
     </main>
+    // <main className="container">
+    //   <h1>Knot Chat Terminal</h1>
+
+    //   <div className="status-bar">
+    //     <span>RTT: {rtt !== null ? `${rtt}ms` : "---"}</span>
+    //   </div>
+
+    //   <form
+    //     className="row"
+    //     onSubmit={(e) => {
+    //       e.preventDefault();
+    //       sendCommand();
+    //     }}
+    //   >
+    //     <select value={command} onChange={(e) => setCommand(e.target.value)}>
+    //       <option value="status">Status</option>
+    //       <option value="version">Version</option>
+    //       <option value="connect">Connect (Addr)</option>
+    //       <option value="ping">Ping (PeerId)</option>
+    //     </select>
+
+    //     <input
+    //       onChange={(e) => setArg(e.currentTarget.value)}
+    //       placeholder="Argumento (Address/ID)..."
+    //       value={arg}
+    //     />
+    //     <button type="submit">Enviar</button>
+    //   </form>
+
+    //   <div className="response-log">
+    //     <h3>Historial de red:</h3>
+    //     <ul>
+    //       {responses.map((res, i) => (
+    //         <li key={i}><code>{res}</code></li>
+    //       ))}
+    //     </ul>
+    //   </div>
+    // </main>
   );
 }
 
